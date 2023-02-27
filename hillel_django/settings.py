aa1575f2d64673818e86e86cef52fdf8da96bb53
Vitalii Pavliuk <pavliuk96@gmail.com>
2023-02-27 21:09:51 +0200

    Add celery for Django

--- hillel_django/settings.py
+++ hillel_django/settings.py
@@ -158,3 +158,10 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_TIMEZONE = "GMT"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
