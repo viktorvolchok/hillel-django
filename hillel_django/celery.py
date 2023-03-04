fe6000bf62a596daf318ab83425cc50777b021dc
Viktor <onik.office@gmail.com>
2023-03-04 19:59:01 +0200

    Add Debug task

--- hillel_django/celery.py
+++ hillel_django/celery.py
@@ -16,6 +16,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+# Debug task
 
 @app.task(bind=True)
 def debug_task(self):
